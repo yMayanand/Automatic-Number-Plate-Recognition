c4d9436b984d5c94fafa8f896d23bab22b4d455f
thinkin-machine <you@example.com>
2022-05-13 15:02:55 +0530

    fix loss

--- loss.py
+++ loss.py
@@ -14,16 +14,20 @@ import torch.nn as nn
     return bbox_loss + object_loss"""
 
 criterion1 = nn.SmoothL1Loss(reduction='none')
-criterion2 = nn.BCELoss(reduction='none')
+criterion2 = nn.CrossEntropyLoss(weight=torch.tensor([0., 48.]), reduction='none')
 
 def loss_fn(preds, labels):
     # bbox loss
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     loss = 0
     for i, label in enumerate(labels):
         pos = assign_cell(label)
         a, b = pos
-        loss1 = criterion1(preds[i, :4, a, b], label[:4])
-        loss2 = criterion2(torch.sigmoid(preds[i, 4, a, b]), label[4])
-        loss += loss1 + loss2
+        obj = criterion1(preds[i, :4, a, b], label[:4])
+        temp = torch.zeros(1, 7, 7).to(device, dtype=torch.long)
+        temp[:, a, b] = torch.tensor([1])
+        loss2 = criterion2(preds[i, 4:, :, :].reshape(2, 49).permute(1, 0), temp.reshape(-1))
+        #conf = criterion2(torch.sigmoid(preds[i, 4, a, b]), label[4])
+        loss += obj + loss
 
     return loss.mean(dim=0)
