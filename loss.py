86c981d37fc1e99770032cc6b691daa70e265844
thinkin-machine <you@example.com>
2022-05-13 15:26:17 +0530

    fix

--- loss.py
+++ loss.py
@@ -13,8 +13,9 @@ import torch.nn as nn
     object_loss = alpha * F.binary_cross_entropy(torch.sigmoid(preds[:, 4, :, :]), labels[:, 4, :, :])
     return bbox_loss + object_loss"""
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 criterion1 = nn.SmoothL1Loss()
-criterion2 = nn.CrossEntropyLoss(weight=torch.tensor([0., 48.]))
+criterion2 = nn.CrossEntropyLoss(weight=torch.tensor([0., 48.]).to(device))
 
 def loss_fn(preds, labels):
     # bbox loss
@@ -26,10 +27,7 @@ def loss_fn(preds, labels):
         obj = criterion1(preds[i, :4, a, b], label[:4])
         temp = torch.zeros(1, 7, 7, dtype=torch.long).to(device)
         temp[:, a, b] = torch.tensor([1]).to(device)
-        print(temp.device)
-        temp1 = preds[i, 4:, :, :].reshape(2, 49).permute(1, 0).to(device)
-        temp2 = temp.reshape(-1).to(device)
-        loss2 = criterion2(temp1, temp2)
+        loss2 = criterion2(preds[i, 4:, :, :].reshape(2, 49).permute(1, 0), temp.reshape(-1))
         #conf = criterion2(torch.sigmoid(preds[i, 4, a, b]), label[4])
         loss += obj + loss2
 
