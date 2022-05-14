54d1529ead3b0a524116b5539070cc15b0f82d90
thinkin-machine <you@example.com>
2022-05-14 14:30:42 +0530

    fix model

--- model.py
+++ model.py
@@ -3,18 +3,16 @@ import torch.nn as nn
 from torchvision import models
 
 class Model(nn.Module):
-    def __init__(self, pretrained=False):
+    def __init__(self, backbone, out_channels):
         super().__init__()
-        model = models.resnet18(pretrained=pretrained)
-        model = list(model.children())[:-2]
-        model.append(nn.AdaptiveAvgPool2d(7))
-        
-        self.relu = nn.ReLU()
-        self.model = nn.Sequential(*model)
-        self.pred = nn.Conv2d(512, 5, 1)
+        self.model = backbone
+        self.pred = nn.Conv2d(out_channels, 5, 1)
 
     def forward(self, x):
         x = self.model(x)
         x = self.pred(x)
         return x
         
+#model = models.resnet18(pretrained=pretrained)
+#model = list(model.children())[:-2]
+#model.append(nn.AdaptiveAvgPool2d(7))
